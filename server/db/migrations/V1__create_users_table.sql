7567ee1ba64adc7e903e16a4bfb03498421a2196
Tesla <230045@astanait.edu.kz>
2024-12-23 14:54:38 +0500

    Added migration table

---
+++ server/db/migrations/V1__create_users_table.sql
@@ -0,0 +1,9 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS users 
+(
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    email varchar(255) NOT NULL,
+    name varchar(255) NOT NULL,
+    password varchar(255) NOT NULL
+)
